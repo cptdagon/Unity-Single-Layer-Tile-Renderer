7e7e4c1e02d216c0fe49df3422f668ab3d1445a1
Adam Rushby <adam.b.rushby@gmail.com>
2019-01-04 15:16:02 +0000

    Update FOWTilingEngine.cs

--- tiling test/Assets/scripts/FOWTilingEngine.cs
+++ tiling test/Assets/scripts/FOWTilingEngine.cs
@@ -1,16 +1,14 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class FOWTilingEngine : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () {		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () {	
 	}
 }
